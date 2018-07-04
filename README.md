fdc7e5dbdc459a69290e61e403014988d180118a
Flkalas <rectifying@gmail.com>
2018-07-05 01:24:44 +0900

    v2.0.0.0
    - Import from firefox version

---
+++ README.md
@@ -0,0 +1,13 @@
+# Twitter Video Assist For Chrome
+
+<https://chrome.google.com/webstore/detail/twitter-video-assist/cledppeceojodgghbbkaciochldmpdfk>
+
+##Firefox Version
+
+<https://addons.mozilla.org/en-US/firefox/addon/twitter-video-assist-ff>
+
+<https://github.com/Flkalas/TwitterVideoAssistFirefox>
+
+###Previous version of Twitter Video Assist For Chrome
+
+<https://github.com/Flkalas/M2G>
---
+++ src/options.js
@@ -0,0 +1,48 @@
+function restoreOptions() {
+	chrome.storage.sync.get({
+		spcificPathName: false,
+		isConvertGIF: true,
+		isSaveMP4: true,
+		isVideoSaveAsMP4: true,
+		isVideoSaveAsTS: true
+	}, onGot);
+}
+
+function onGot(items) {
+	document.getElementById('specific').checked = items.spcificPathName;
+	document.getElementById('gifasgif').checked = items.isConvertGIF;
+	document.getElementById('gifasmp4').checked = items.isSaveMP4;
+	document.getElementById('videoasmp4').checked = items.isVideoSaveAsMP4;
+	document.getElementById('videoasts').checked = items.isVideoSaveAsTS;
+}
+
+function setItem() {
+	var status = document.getElementById('status');
+	status.textContent = 'Options saved.';
+	setTimeout(function () {
+		status.textContent = "";
+	}, 750);
+}
+
+function saveOptions() {
+	var specific = document.getElementById('specific').checked;
+	var convertGIF = document.getElementById('gifasgif').checked;
+	var saveMP4 = document.getElementById('gifasmp4').checked;
+	var saveVideoMP4 = document.getElementById('videoasmp4').checked;
+	var saveVideoTS = document.getElementById('videoasts').checked;
+
+	chrome.storage.sync.set({
+		spcificPathName: specific,
+		isConvertGIF: convertGIF,
+		isSaveMP4: saveMP4,
+		isVideoSaveAsMP4: saveVideoMP4,
+		isVideoSaveAsTS: saveVideoTS
+	}, setItem);
+}
+
+document.getElementById('specific').addEventListener("change", saveOptions);
+document.getElementById('gifasgif').addEventListener("change", saveOptions);
+document.getElementById('gifasmp4').addEventListener("change", saveOptions);
+document.getElementById('videoasmp4').addEventListener("change", saveOptions);
+document.getElementById('videoasts').addEventListener("change", saveOptions);
+document.addEventListener('DOMContentLoaded', restoreOptions);
